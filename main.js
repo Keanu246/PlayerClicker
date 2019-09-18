6a8bf49062eec701273a4f542fcb72a811da1ec2
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-18 18:40:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,18 +19,8 @@ function buyBalon(){
     document.getElementById('balonCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
-function balonFootballs(){
-    var balonKicks = Math.floor(1 * Math.pow(1.08,balontelli));
-    if(footballs >= balonKicks){
-       footballs = footballs - balonKicks;
-       document.getElementById('balonKicks').innerHTML = balonKicks;
-       document.getElementById('balontelli').innerHTML = balontelli;
-       document.getElementById('footballs').innerHTML = footballs;
-    };
-};
-
 window.setInterval(function(){
 	
-	balonFootballs(balontelli);
+	footballClick(balontelli);
 	
 }, 5000);
