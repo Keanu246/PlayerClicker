a69fa04d3b951a04b7aa0615e505419cf9330c4b
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-18 18:03:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,15 +8,15 @@ function footballClick(number){
 var balontelli = 0;
 
 function buyBalon(){
-    var balonCost = Math.floor(15 * Math.pow(1.15,balontelli));     //works out the cost of this cursor
-    if(footballs >= balonCost){                                   //checks that the player can afford the cursor
+    var balontelliCost = Math.floor(15 * Math.pow(1.15,balontelli));     //works out the cost of this cursor
+    if(footballs >= balontelliCost){                                   //checks that the player can afford the cursor
         balontelli = balontelli + 1;                                   //increases number of cursors
-    	footballs = footballs - balonCost;                          //removes the cookies spent
+    	footballs = footballs - balontelliCost;                          //removes the cookies spent
         document.getElementById('balontelli').innerHTML = balontelli;  //updates the number of cursors for the user
         document.getElementById('footballs').innerHTML = footballs;  //updates the number of cookies for the user
     };
     var nextCost = Math.floor(15 * Math.pow(1.15,balontelli));       //works out the cost of the next cursor
-    document.getElementById('balonCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    document.getElementById('balontelliCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
