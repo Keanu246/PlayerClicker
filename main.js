b81a83aa96dcaa68797d9d7e583cafd42a6aa140
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-09-18 17:53:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,18 +5,18 @@ function footballClick(number){
     document.getElementById("footballs").innerHTML = footballs;
 };
 
-var cursors = 0;
+var balon = 0;
 
-function buyCursor(){
-    var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(cookies >= cursorCost){                                   //checks that the player can afford the cursor
-        cursors = cursors + 1;                                   //increases number of cursors
-    	cookies = cookies - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = cursors;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+function buyBalon(){
+    var balonCost = Math.floor(15 * Math.pow(1.15,balon));     //works out the cost of this cursor
+    if(footballs >= balonCost){                                   //checks that the player can afford the cursor
+        balon = balon + 1;                                   //increases number of cursors
+    	footballs = footballs - balonCost;                          //removes the cookies spent
+        document.getElementById('balon').innerHTML = balon;  //updates the number of cursors for the user
+        document.getElementById('footballs').innerHTML = footballs;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var nextCost = Math.floor(15 * Math.pow(1.15,balon));       //works out the cost of the next cursor
+    document.getElementById('balonCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
